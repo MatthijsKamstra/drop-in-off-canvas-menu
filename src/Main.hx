617b904741523bc83d79b4e547fead6b180128de
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-11-08 22:35:10 +0100

    removed icon, removed monkee style, added css

--- src/Main.hx
+++ src/Main.hx
@@ -10,21 +10,14 @@ class Main {
 	public function new() {
 		trace('cc-nav: a drop in off-canvas menu');
 
-		// ficons add to document
-		var link = document.createLinkElement();
-		link.href = "https://cdn.jsdelivr.net/npm/ficons@1.1.52/dist/ficons/font.css";
-		link.rel = "stylesheet";
-		document.head.appendChild(link);
-
-		// for now monkee.style is added as well
-		var link = document.createLinkElement();
-		link.href = "https://matthijskamstra.github.io/monkee-style/dist/monkee.css";
-		link.rel = "stylesheet";
-		document.head.appendChild(link);
+		var css = ":root{--black:#212121;--white:#ffffff;--sidebar-width:250px;--icon-padding:20px;--icon-left:20px}body{margin:0;padding:0}.slideout-sidebar{font-family:Arial,Helvetica,sans-serif;z-index:0;position:fixed;top:0;left:calc(-1 * var(--sidebar-width));width:var(--sidebar-width);height:100%;background-color:var(--black);transition:.3s ease-in-out;overflow:scroll;padding:0;margin:0}.slideout-sidebar a{color:var(--white);outline:0;text-decoration:none;font-size:.8rem}.slideout-sidebar ul{list-style:none;margin:0;padding:20px}.slideout-sidebar ul li{cursor:pointer;padding:18px 0;border-bottom:1px solid rgba(244,244,244,.4)}.slideout-sidebar ul li:last-child{border-bottom:0}#menu-toggle{display:none}.menu-icon{position:absolute;top:0;left:0;font-size:30px;z-index:20000;transition:.3s ease-in-out;padding:var(--icon-padding);cursor:pointer}#menu-toggle:checked~.slideout-sidebar{left:0}#menu-toggle:checked+.menu-icon{left:calc(var(--icon-left) + var(--sidebar-width))}";
+		var style = document.createStyleElement();
+		style.innerText = css;
+		document.head.appendChild(style);
 
 		// basic stuff for menu at the top of body
 		var input = htmlToElement('<input type="checkbox" id="menu-toggle" />');
-		var label = htmlToElement('<label for="menu-toggle" class="menu-icon"><i class="fa fa-bars"></i></label>');
+		var label = htmlToElement('<label for="menu-toggle" class="menu-icon">&#9776;</label>');
 		document.body.prepend(label);
 		document.body.prepend(input);
 
